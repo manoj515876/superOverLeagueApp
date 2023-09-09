cc48392ecd48738bc954d51276b407d83fef965f
Manoj Mantha <manojputtaluckey@gmail.com>
2023-09-09 10:55:56 +0000

    Super Over App

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="container">
+    <div className="card">
+      <h1 className="heading">Super Over League</h1>
+      <div className="img-card">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
